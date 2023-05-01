6f40b97c20ca6224c6f92c00e6e0bffcd0b976ff
sebastianvillarrealsc <sebastianvillarealsc@gmail.com>
2023-05-01 12:49:48 -0300

    EJECICIO

---
+++ index.ts
@@ -0,0 +1,7 @@
+import { calcularPotencia } from "./ejercicio1";
+import { esMultiplo } from "./ejercicio2";
+import { cantidadDeDivisores } from "./ejercicio3";
+
+calcularPotencia;
+esMultiplo;
+cantidadDeDivisores
